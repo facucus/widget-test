f3a8c4f734c6c7b5281753a5e650be0796a503d1
fmechetti <fmechetti@gmail.com>
2020-12-10 12:21:55 -0500

    chattt

--- src/App.js
+++ src/App.js
@@ -1,68 +1,23 @@
 import React from "react"
-import logo from './logo.svg';
-import './App.css';
-
-function renderPost(post) {
-  const { data: { title, url, author, id } } = post;
-  const authorUrl = `https://www.reddit.com/u/${author}`
-
-  return (
-    <div className="reddit_widget__post" key={id}>
-      <div className="reddit_widget__posted_by">
-        posted by <a href={authorUrl} className="reddit_widget__posted_by" target="_blank" rel="noopener noreferrer">u/{author}</a>
-      </div>
-      <a href={url} className="reddit_widget__title" target="_blank" rel="noopener noreferrer">{title}</a>
-    </div>
-  )
-}
-
-function nonStickiedOnly(post) {
-  return !post.data.stickied
-}
 
 function App({ domElement }) {
-  const subreddit = domElement.getAttribute("data-subreddit")
-  const [loading, setLoading] = React.useState();
-  const [error, setError] = React.useState();
-  const [data, setData] = React.useState([]);
-
-  React.useEffect(() => {
-    setLoading(true);
-    fetch(`https://www.reddit.com/r/${subreddit}.json`)
-      .then((response) => response.json())
-      .then((data) => {
-        setLoading(false);
-        setData(data.data.children.slice(0, 10));
-      })
-      .catch((e) => {
-        console.log(e)
-        setLoading(false);
-        setError('error fetching from reddit');
-      });
-  }, [subreddit])
-
+  const room = domElement.getAttribute("data-room")
   return (
-    <div className="reddit_widget__app">
-      <h1 className="reddit_widget__header">
-        Latest posts in <a href={`https://reddit.com/r/${subreddit}`} rel="noopener noreferrer">/r/{subreddit}</a>
-      </h1>
-      <div className="reddit_widget__inner">
-        {loading && "Loading..."}
-        {error && error}
-        {!!data.length && data.filter(nonStickiedOnly).map(renderPost)}
-      </div>
-      <p className="reddit_widget__powered_by">
-        This widget is powered by{" "}
-        <a
-          href="https://javascriptpros.com"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          JavaScriptPros.com
-        </a>
-      </p>
+    <div className="bspk_widget__chat">
+      <iframe style={styles.iframe} src={`https://14611c33125a.ngrok.io/widget/welcome?room=${room}`} title="Widget chat"></iframe>
     </div>
   );
+}
+
+
+const styles = {
+  iframe: {
+    width: '400px',
+    height: '400px',
+    position: 'absolute',
+    bottom: '20px',
+    right: '20px'
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const WidgetsDivs = document.querySelectorAll('.reddit_widget')
+const WidgetsDivs = document.querySelectorAll('.bspk-chat-widget')
 
 WidgetsDivs.forEach(Div => {
-  console.log("lalala")
-  console.log('Div', Div)
   ReactDOM.render(
     <React.StrictMode>
       <App domElement={Div}/>
